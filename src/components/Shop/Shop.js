61ab6e3dbeef52cf981453436d079d8f26008501
Nayem-khan1 <nayemkhan4496@gmail.com>
2022-12-31 09:12:38 +0600

    Cart file added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Products from '../Products/Products';
 import './Shop.css'
 
@@ -26,8 +27,7 @@ const Shop = () => {
                 }
             </div>
             <div className='shopping-cart'>
-                <h3>Order summary</h3>
-                <p>Selected Item: {cart.length}</p>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
